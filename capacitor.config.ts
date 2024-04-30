887742dd6ca2b2eca33f68ad45d616447d812397
Cristian Ramos <tcristian_15.go@hotmail.com>
2024-04-30 13:04:56 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'loaders',
+  webDir: 'www'
+};
+
+export default config;
